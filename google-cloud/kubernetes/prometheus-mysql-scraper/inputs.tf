a9781d427f9d148e9091cda6dc312569939b61e9
Jason Tevnan <jason.tevnan@cabify.com>
2018-07-10 11:13:53 +0200

    Defines the input variables for the moduel

---
+++ google-cloud/kubernetes/prometheus-mysql-scraper/inputs.tf
@@ -0,0 +1,24 @@
+variable "service_name" {
+  description = "Name of the service - will be used for instance names"
+  type        = "string"
+}
+
+variable "user_name" {
+  description = "Username for the db (16 char max length)"
+  type        = "string"
+}
+
+variable "user_password" {
+  description = "Clear text password for db user"
+  type        = "string"
+}
+
+variable "instance_region" {
+  description = "Where to start the instances"
+  type        = "string"
+}
+
+variable "project" {
+  description = "Name of the gcp project"
+  type        = "string"
+}
