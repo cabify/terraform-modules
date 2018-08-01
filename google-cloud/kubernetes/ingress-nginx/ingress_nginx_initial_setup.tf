bf62163247c2c5887e4d804b87c5fe3b6b780866
Jesus Vazquez <jesus.vazquez@cabify.com>
2018-08-01 16:11:56 +0200

    Replace mandatory local exec with template

--- google-cloud/kubernetes/ingress-nginx/ingress_nginx_initial_setup.tf
+++ google-cloud/kubernetes/ingress-nginx/ingress_nginx_initial_setup.tf
@@ -1,6 +1,8 @@
-# Run the mandatory manifests https://kubernetes.github.io/ingress-nginx/deploy/#mandatory-command
-resource "null_resource" "ingress_nginx_mandatory_setup" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml"
-  }
+# Runs the mandatory manifests https://kubernetes.github.io/ingress-nginx/deploy/#mandatory-command
+data "template_file" "ingress_nginx_mandatory_template" {
+  template = "${file("mandatory.yaml")}"
+}
+
+resource "k8s_manifest" "ingress_nginx_mandatory" {
+  content = "${data.template_file.ingress_nginx_mandatory_template.rendered}"
 }
