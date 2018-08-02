596ad0ebfa45c951e771ef4cf0b797712025f1c3
Jason Tevnan <jason.tevnan@cabify.com>
2018-08-02 13:12:45 +0200

    Initial commit of elastic search module

---
+++ google-cloud/kubernetes/prometheus-elasticsearch-scraper/replication-controller.tf
@@ -0,0 +1,59 @@
+//We need this until https://github.com/terraform-providers/terraform-provider-kubernetes/pull/101 can be merged
+
+resource "kubernetes_replication_controller" "elasticsearch" {
+  metadata {
+    name = "elasticsearch-scraper"
+
+    labels {
+      app = "elasticsearch-scraper"
+    }
+
+    namespace = "${var.namespace}"
+  }
+
+  spec {
+    selector {
+      app = "elasticsearch-scraper"
+    }
+
+    template {
+      restart_policy = "Always"
+
+      node_selector {
+        "cloud.google.com/gke-nodepool" = "gke-prometheus"
+      }
+
+      container {
+        image = "justwatch/elasticsearch_exporter"
+        name  = "elasticsearch-exporter"
+
+        port {
+          container_port = "${var.container_port}"
+        }
+
+        env {
+          name = "URI"
+
+          value_from {
+            secret_key_ref {
+              name = "${kubernetes_secret.elasticsearch.metadata.0.name}"
+              key  = "uri"
+            }
+          }
+        }
+
+        args = ["-es.uri=$URI"]
+
+        liveness_probe {
+          http_get {
+            path = "/"
+            port = "${var.container_port}"
+          }
+
+          initial_delay_seconds = 5
+          period_seconds        = 3
+        }
+      }
+    }
+  }
+}
---
+++ google-cloud/kubernetes/prometheus-elasticsearch-scraper/service.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_service" "elasticsearch" {
+  metadata {
+    annotations {
+      prometheus_io_scrape = "true"
+    }
+
+    name      = "${kubernetes_replication_controller.elasticsearch.metadata.0.name}"
+    namespace = "${var.namespace}"
+  }
+
+  spec {
+    selector {
+      app = "${kubernetes_replication_controller.elasticsearch.metadata.0.labels.app}"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = "${var.container_port}"
+      target_port = "${var.container_port}"
+    }
+
+    type = "ClusterIP"
+  }
+}
