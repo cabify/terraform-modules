f307238d756317e753311d44cdcbba596218cedf
Jesus Vazquez <jesusvzpg@gmail.com>
2018-08-02 11:53:14 +0200

    Initial setup for the oauth2 proxy module

---
+++ google-cloud/kubernetes/oauth2-proxy/inputs.tf
@@ -0,0 +1,36 @@
+###############################################################################
+## Oauth2 Proxy Configuration
+###############################################################################
+variable "oauth2_proxy_client_id" {
+  type = "string"
+}
+
+variable "oauth2_proxy_client_secret" {
+  type = "string"
+}
+
+variable "oauth2_proxy_cookie" {
+  type = "string"
+}
+
+###############################################################################
+## Oauth2 Proxy Deployment
+###############################################################################
+
+variable "oauth2_proxy_deployment_namespace" {
+  type    = "string"
+  default = "ingress-nginx"
+}
+
+variable "oauth2_proxy_deployment_replicas" {
+  type    = "string"
+  default = "1"
+}
+
+variable "oauth2_proxy_deployment_image" {
+  type = "string"
+}
+
+variable "oauth2_proxy_deployment_email_domain" {
+  type = "string"
+}
