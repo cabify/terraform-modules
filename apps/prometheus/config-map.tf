b9415d3c90221b676857c5e6420ed5e5482abf01
Jason Tevnan <jason.tevnan@cabify.com>
2018-08-21 10:28:51 +0200

    Adds creation of empty configmaps for alert/recording rules and cleans up prometheus config

--- apps/prometheus/config-map.tf
+++ apps/prometheus/config-map.tf
@@ -1,3 +1,27 @@
+variable "configmaps" {
+  type = "string"
+
+  default = <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: alertrules
+  namespace: prometheus
+data:
+  placeholder: /
+    empty
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: recordingrules 
+  namespace: prometheus
+data:
+  placeholder: /
+    empty
+EOF
+}
+
 resource "kubernetes_config_map" "prometheus-config-map" {
   metadata {
     name      = "prometheus-configuration"
@@ -5,150 +29,16 @@ resource "kubernetes_config_map" "prometheus-config-map" {
   }
 
   data {
-    prometheus.yml = <<EOF
-    global:
-      scrape_interval: 15s
-      scrape_timeout: 15s
-      evaluation_interval: 1m
+    prometheus.yml = "${file("${path.module}/files/prometheus.yaml")}"
+  }
 
-    scrape_configs:
+  // Create empty configmaps to be filled by CI
+  provisioner "local-exec" {
+    command = "cat <<EOF | kubectl create -f - ${var.configmaps}EOF"
+  }
 
-      - job_name: 'kubernetes-apiservers'
-        kubernetes_sd_configs:
-        - role: endpoints
-        scheme: https
-        tls_config:
-          ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-        bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
-        relabel_configs:
-        - source_labels: [__meta_kubernetes_namespace, __meta_kubernetes_service_name, __meta_kubernetes_endpoint_port_name]
-          action: keep
-          regex: default;kubernetes;https
-
-      - job_name: 'kubernetes-pods'
-        kubernetes_sd_configs:
-        - role: pod
-        relabel_configs:
-        - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_scrape]
-          action: keep
-          regex: true
-        - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_path]
-          action: replace
-          target_label: __metrics_path__
-          regex: (.+)
-        - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
-          action: replace
-          regex: ([^:]+)(?::\d+)?;(\d+)
-          replacement: $1:$2
-          target_label: __address__
-        - action: labelmap
-          regex: __meta_kubernetes_pod_label_(.+)
-        - source_labels: [__meta_kubernetes_namespace]
-          action: replace
-          target_label: kubernetes_namespace
-        - source_labels: [__meta_kubernetes_pod_name]
-          action: replace
-          target_label: kubernetes_pod_name
-      - job_name: 'kubernetes-cadvisor'
-        scheme: https
-        tls_config:
-          ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-        bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
-        kubernetes_sd_configs:
-        - role: node
-        relabel_configs:
-        - action: labelmap
-          regex: __meta_kubernetes_node_label_(.+)
-        - target_label: __address__
-          replacement: kubernetes.default.svc:443
-        - source_labels: [__meta_kubernetes_node_name]
-          regex: (.+)
-          target_label: __metrics_path__
-          replacement: /api/v1/nodes/$${1}/proxy/metrics/cadvisor
-
-      - job_name: 'kubernetes-nodes'
-
-        scheme: https
-
-        tls_config:
-          ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-        bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
-
-        kubernetes_sd_configs:
-        - role: node
-
-        relabel_configs:
-        - action: labelmap
-          regex: __meta_kubernetes_node_label_(.+)
-        - target_label: __address__
-          replacement: kubernetes.default.svc:443
-        - source_labels: [__meta_kubernetes_node_name]
-          regex: (.+)
-          target_label: __metrics_path__
-          replacement: /api/v1/nodes/$${1}/proxy/metrics
-
-      
-      - job_name: 'kubernetes-service-endpoints'
-        
-        kubernetes_sd_configs:
-        - role: service
-
-        relabel_configs:
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_scrape]
-          action: keep
-          regex: true
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_scheme]
-          action: replace
-          target_label: __scheme__
-          regex: (https?)
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_path]
-          action: replace
-          target_label: __metrics_path__
-          regex: (.+)
-        - source_labels: [__address__, __meta_kubernetes_service_annotation_prometheus_io_port]
-          action: replace
-          target_label: __address__
-          regex: ([^:]+)(?::\d+)?;(\d+)
-          replacement: $1:$2
-        - action: labelmap
-          regex: __meta_kubernetes_service_label_(.+)
-        - source_labels: [__meta_kubernetes_namespace]
-          action: replace
-          target_label: kubernetes_namespace
-        - source_labels: [__meta_kubernetes_service_name]
-          action: replace
-          target_label: kubernetes_name
-
-      - job_name: 'kubernetes-https-service-endpoints'
-        
-        scheme: https
-        
-        kubernetes_sd_configs:
-        - role: service
-
-        relabel_configs:
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_https_scrape]
-          action: keep
-          regex: true
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_https_scheme]
-          action: replace
-          target_label: __scheme__
-          regex: (https?)
-        - source_labels: [__meta_kubernetes_service_annotation_prometheus_io_https_path]
-          action: replace
-          target_label: __metrics_path__
-          regex: (.+)
-        - source_labels: [__meta_kubernetes_service_annotation_instance]
-          action: replace
-          target_label: __address__
-        - action: labelmap
-          regex: __meta_kubernetes_service_label_(.+)
-        - source_labels: [__meta_kubernetes_namespace]
-          action: replace
-          target_label: kubernetes_namespace
-        - source_labels: [__meta_kubernetes_service_name]
-          action: replace
-          target_label: kubernetes_name
-    EOF
+  provisioner "local-exec" {
+    command = "cat <<EOF | kubectl delete -f - ${var.configmaps}EOF"
+    when    = "destroy"
   }
 }
