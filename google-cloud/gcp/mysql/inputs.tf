1586206df82cc89495d62852c43aa8f17209fdbd
Diego Rabatone Oliveira <diego.rabatone@cabify.com>
2020-01-20 15:24:47 -0300

    First commit for the google-gcp-mysql-ha module

---
+++ google-cloud/gcp/mysql/inputs.tf
@@ -0,0 +1,67 @@
+###############################################################################
+# Variables without default value
+###############################################################################
+variable "gcp_image" {
+  description = "Which image should be used for the VMs."
+  type        = "string"
+}
+
+variable "service_name" {
+  description = "Name of the service to which the cluster will serve."
+  type        = "string"
+}
+
+variable "first_instance_size" {
+  description = "Machine type of the first instance."
+  type        = "string"
+}
+
+variable "replica_instance_size" {
+  description = "Machine type of the other instances"
+  type        = "string"
+}
+
+variable "subnetwork" {
+  description = "Subnetwork to be used for the instances"
+  type        = "string"
+}
+
+variable "user_name" {
+  description = "Username for the db (16 char max length)"
+  type        = "string"
+}
+
+variable "user_password" {
+  description = "Clear text password for db user"
+  type        = "string"
+}
+
+variable "zone_1" {
+  description = "GCP Zone in which the instance 1 should be created."
+  type        = "string"
+}
+
+variable "zone_2" {
+  description = "GCP Zone in which the instance 2 should be created."
+  type        = "string"
+}
+
+variable "zone_3" {
+  description = "GCP Zone in which the instance 3 should be created."
+  type        = "string"
+}
+
+###############################################################################
+# Variables with default value
+###############################################################################
+variable "deletion_protection" {
+  description = "Bool if the instance deletetion_protection is enabled or not."
+  type        = "bool"
+  default     = true
+}
+
+variable "disk_size" {
+  description = "Instances disk size (GB)."
+  type        = "number"
+  default     = 100
+}
