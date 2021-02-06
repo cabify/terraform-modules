198d94f3fcde4178af01bbacceaea3a39aec9d04
Jason Tevnan <jason.tevnan@cabify.com>
2021-02-06 11:41:47 +0100

    removing mysql references for now

--- aws/rds/exporter_secrets.tf
+++ aws/rds/exporter_secrets.tf
@@ -5,7 +5,8 @@ resource "kubernetes_secret" "rds" {
   }
 
   data = {
-    exporter_username = mysql_user.exporter.user
+    //exporter_username = mysql_user.exporter.user
+    exporter_username = "exporter"
     exporter_password = var.exporter_password
     exporter_hostname = aws_db_instance.primary.address
   }
