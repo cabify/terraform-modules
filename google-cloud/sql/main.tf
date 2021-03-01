7791f536857c23b2bd871f3e1804128f05b5c877
Jason Tevnan <jason.tevnan@cabify.com>
2021-03-01 10:36:31 +0100

    removes exporters everywhere

--- google-cloud/sql/main.tf
+++ google-cloud/sql/main.tf
@@ -95,19 +95,3 @@ resource "google_sql_database_instance" "google_sql_database_instance-module-rea
   count      = var.instance_read_only_replica_count
   depends_on = [google_sql_database_instance.google_sql_database_instance-module-master]
 }
-
-module "cabify_prometheus_mysql_scraper" {
-  source                           = "github.com/cabify/terraform-modules.git//google-cloud/kubernetes/prometheus-mysql-scraper?ref=google-cloud_kubernetes-prometheus-mysql-scraper-v0.1.19"
-  service_name                     = var.service_name
-  user_name                        = var.user_name
-  user_password                    = var.user_password
-  instance_region                  = var.instance_region
-  project                          = var.project
-  namespace                        = var.namespace
-  owner                            = var.owner
-  tier                             = var.tier
-  instance_read_only_replica_count = var.instance_read_only_replica_count
-  instance_tier                    = var.instance_tier
-  instance_tier_read_only_replica  = var.instance_tier_read_only_replica
-  prometheus-scrape-flag           = var.prometheus-scrape-flag
-}
