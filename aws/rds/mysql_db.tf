51f317f34f1d34390b44f91208349585e0fe62af
Paco Guzmán (BE Programmer) <paco.guzman@cabify.com>
2021-03-18 16:03:06 +0100

    mysql-database default charset and collation

--- aws/rds/mysql_db.tf
+++ aws/rds/mysql_db.tf
@@ -2,4 +2,6 @@
 # by the aws_db_instance resource above.
 resource "mysql_database" "appdb" {
   name = var.service_name == "UNSET" ? var.instance_name : var.service_name
+  default_character_set = "utf8mb4"
+  default_collation = "utf8mb4_general_ci"
 }
