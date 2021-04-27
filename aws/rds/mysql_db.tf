b753031469d401410ddc0198d1ebf7483ab81bbf
Paco Guzmán (BE Programmer) <paco.guzman@cabify.com>
2021-04-27 13:11:27 +0200

    mysql_db instance name must be the instance_name

--- aws/rds/mysql_db.tf
+++ aws/rds/mysql_db.tf
@@ -1,7 +1,7 @@
 # Create a second database, in addition to the "initial_db" created
 # by the aws_db_instance resource above.
 resource "mysql_database" "appdb" {
-  name = var.service_name == "UNSET" ? var.instance_name : var.service_name
+  name = var.instance_name
   default_character_set = "utf8mb4"
   default_collation = "utf8mb4_general_ci"
 }
