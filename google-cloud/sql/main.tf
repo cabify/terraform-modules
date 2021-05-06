45e1d5d5cd5adb2ffaf146b27c17f54ee940dd52
Leonardo Sorrentino <leonardo.sorrentino@cabify.com>
2021-05-06 18:31:31 +0200

    remove not needed code

--- google-cloud/sql/main.tf
+++ google-cloud/sql/main.tf
@@ -6,13 +6,6 @@ resource "google_sql_database_instance" "google_sql_database_instance-module-mas
 
   settings {
 
-    ip_configuration {
-      authorized_networks {
-        name  = "dms"
-        value = "${var.dms_instances[0][0]}/32"
-      }
-    }
-
     tier = var.instance_tier
 
     disk_autoresize = var.instance_disk_autoresize
