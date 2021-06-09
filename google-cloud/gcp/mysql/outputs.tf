804e532a22a39d892d4d3010ca71b0b316f48b87
Diego Rabatone Oliveira <diego.rabatone@cabify.com>
2021-06-09 16:31:55 -0300

    Update google-cloud/gcp/mysql module
    Updating to terraform 1.0.0.

--- google-cloud/gcp/mysql/outputs.tf
+++ google-cloud/gcp/mysql/outputs.tf
@@ -2,13 +2,13 @@
 ## Instances Names
 ################################################################################
 output "mysqlcluster_first_name" {
-  value = "${google_compute_instance.mysqlcluster-module_first.name}"
+  value = google_compute_instance.mysqlcluster-module_first.name
 }
 
 output "mysqlcluster_second_name" {
-  value = "${google_compute_instance.mysqlcluster-module_second.name}"
+  value = google_compute_instance.mysqlcluster-module_second.name
 }
 
 output "mysqlcluster_third_name" {
-  value = "${google_compute_instance.mysqlcluster-module_third.name}"
+  value = google_compute_instance.mysqlcluster-module_third.name
 }
